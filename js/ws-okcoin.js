81c82211a8505bad30a8a03753e3b3981a2c5553
nyg <nyg>
2017-06-17 16:55:05 +0200

    OKCoin futures works again.

--- js/ws-okcoin.js
+++ js/ws-okcoin.js
@@ -1,12 +1,15 @@
-new OKCoin('com', {
+new OKCoin()
+.setChannels({
     ok_sub_futureusd_btc_index: handleIndex,
     ok_sub_futureusd_btc_trade_this_week: handleTrade,
     ok_sub_futureusd_btc_trade_next_week: handleTrade,
     ok_sub_futureusd_btc_trade_quarter: handleTrade,
-    ok_sub_futureusd_btc_ticker_this_week: handleTicker,
-    ok_sub_futureusd_btc_ticker_next_week: handleTicker,
-    ok_sub_futureusd_btc_ticker_quarter: handleTicker
-}).start()
+    ok_sub_future_btc_ticker_this_week: handleTicker,
+    ok_sub_future_btc_ticker_next_week: handleTicker,
+    ok_sub_future_btc_ticker_quarter: handleTicker
+})
+.isFutures()
+.start()
 
 var okcoin = exchanges.okcoin,
     channelToProduct = {
@@ -15,16 +18,19 @@ var okcoin = exchanges.okcoin,
         quarter: okcoin.products.quarterlies
     }
 
+// Index data
 function handleIndex(message) {
     view.updateIndex(okcoin.name, message.data.futureIndex)
 }
 
+// Last trade data
 function handleTrade(message) {
     message.data.forEach(function (trade) {
         view.update(okcoin.name, getProduct(message.channel), 'price', parseFloat(trade[1]))
     })
 }
 
+// Volume data
 function handleTicker(message) {
     var volume = parseFloat(message.data.vol) / 10 // in k$
     view.updateVolume(okcoin.name, getProduct(message.channel), volume)
